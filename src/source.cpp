03ef69826db008396fdca86a99b7d7c762dacba7
Drew Borneman <borneman@case.edu>
2019-10-01 17:14:56 -0400

    started working on reading the commands

--- src/source.cpp
+++ src/source.cpp
@@ -1,66 +1,34 @@
 // Include the header file for Trigger.
 #include "std_srvs/Trigger.h"
 #include "ros/ros.h"
+#include "trajectory_msgs/JointTrajectory.h"
 #include "std_msgs/String.h"
 
-void chatterCallback(const std_msgs::String::ConstPtr& msg)
+void recieveOrder(std::vector<trajectory_msgs::JointTrajectory> trajectory_vector)
 {
-  ROS_INFO("I heard: [%s]", msg->data.c_str());
+  ROS_INFO("I heard: [%s]", trajectory_vector);
 }
 
 int main(int argc, char **argv)
 {
-  /**
-   * The ros::init() function needs to see argc and argv so that it can perform
-   * any ROS arguments and name remapping that were provided at the command line.
-   * For programmatic remappings you can use a different version of init() which takes
-   * remappings directly, but for most command-line programs, passing argc and argv is
-   * the easiest way to do it.  The third argument to init() is the name of the node.
-   *
-   * You must call one of the versions of ros::init() before using any other
-   * part of the ROS system.
-   */
-  ros::init(argc, argv, "listener");
+  ros::init(argc, argv, "ariac_challenge");
 
-  /**
-   * NodeHandle is the main access point to communications with the ROS system.
-   * The first NodeHandle constructed will fully initialize this node, and the last
-   * NodeHandle destructed will close down the node.
-   */
   ros::NodeHandle n;
 
-  /**
-   * The subscribe() call is how you tell ROS that you want to receive messages
-   * on a given topic.  This invokes a call to the ROS
-   * master node, which keeps a registry of who is publishing and who
-   * is subscribing.  Messages are passed to a callback function, here
-   * called chatterCallback.  subscribe() returns a Subscriber object that you
-   * must hold on to until you want to unsubscribe.  When all copies of the Subscriber
-   * object go out of scope, this callback will automatically be unsubscribed from
-   * this topic.
-   *
-   * The second parameter to the subscribe() function is the size of the message
-   * queue.  If messages are arriving faster than they are being processed, this
-   * is the number of messages that will be buffered up before beginning to throw
-   * away the oldest ones.
-   */
-  ros::Subscriber sub = n.subscribe("chatter", 1000, chatterCallback);
+  ros::Subscriber sub = n.subscribe("ariac/arm/command", 1000, recieveOrder);
 
   // To declare the variable in this way where necessary in the code. 
   std_srvs::Trigger begin_comp;
   // Create the service client.
-  ros::ServiceClient begin_client = n.serviceClient<std_srvs::Trigger>("Service Name");
+  ros::ServiceClient begin_client = n.serviceClient<std_srvs::Trigger>("ariac_challenge_service");
   // Call the Service
   begin_client.call(begin_comp);
   // Sample output
   ROS_WARN("Competition service returned failure: %s",begin_comp.response.message.c_str());
-  return 0;
 
-  /**
-   * ros::spin() will enter a loop, pumping callbacks.  With this version, all
-   * callbacks will be called from within this thread (the main one).  ros::spin()
-   * will exit when Ctrl-C is pressed, or the node is shutdown by the master.
-   */
+  std::vector<trajectory_msgs::JointTrajectory> trajectory_vector;
+  trajectory_vector.clear();
+
   ros::spin();
 
   return 0;
