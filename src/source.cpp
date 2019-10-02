abe574f86a1330a9976982fd2654f632d362b40d
Drew Borneman <awb79@case.edu>
2019-10-02 19:13:11 -0400

    all code in lab pdf

--- src/source.cpp
+++ src/source.cpp
@@ -20,6 +20,9 @@ std::string ObjectType = "piston_rod_part";
 tf2_ros::Buffer tfBuffer;
 moveit::planning_interface::MoveGroupInterface move_group("manipulator");
 
+	//create variables
+	geometry_msgs::PoseStamped current_pose, end_pose;
+
 void recieveOrder(const osrf_gear::Order::ConstPtr & order)
 {
 
@@ -31,6 +34,8 @@ void logicalCameraCallback(const osrf_gear::LogicalCameraImage::ConstPtr & image
 { 
   for(int i=0;i<imageMsg->models.size();i++){
     ROS_INFO("Model: %s\n",imageMsg->models[i]);
+		if(imageMsg->models[i]->type == ObjectType)
+			current_pose.pose = imageMsg->models[i]->pose;
   }
 }
 
@@ -64,29 +69,36 @@ int main(int argc, char **argv)
   order_vector.clear();
 
   startCompetition(n);
-//Retrieve the transformation
-geometry_msgs::TransformStamped tfStamped;
-try {
-  tfStamped = tfBuffer.lookupTransform(move_group.getPlanningFrame().c_str(), "logical_camera_frame", ros::Time(0.0), ros::Duration(1.0));
-  ROS_DEBUG("Transform to [%s] from [%s]", tfStamped.header.frame_id.c_str(), tfStamped.child_frame_id.c_str());
-  }
-catch (tf2::TransformException &ex) {
-  ROS_ERROR("%s", ex.what());
-  }
- //tf2_ros::Buffer.lookupTransform("to_frame", "from_frame", "how_recent", "how_long_to_wait");
+	while(order_vector.size() > 0){
+		//Retrieve the transformation
+		geometry_msgs::TransformStamped tfStamped;
+		try {
+			tfStamped = tfBuffer.lookupTransform(move_group.getPlanningFrame().c_str(), "logical_camera_frame", ros::Time(0.0), ros::Duration(1.0));
+			ROS_DEBUG("Transform to [%s] from [%s]", tfStamped.header.frame_id.c_str(), 		tfStamped.child_frame_id.c_str());
+			}
+		catch (tf2::TransformException &ex) {
+			ROS_ERROR("%s", ex.what());
+		}
+		 //tf2_ros::Buffer.lookupTransform("to_frame", "from_frame", "how_recent", "how_long_to_wait");
 
-//create variables
-geometry_msgs::PoseStamped current_pose, end_pose;
-//current_pose.pose = //...;  unsure what to do here 
+		end_pose.pose.position.z += 0.10;
+		end_pose.pose.orientation.w = 0.707;
+		end_pose.pose.orientation.x = 0.0;
+		end_pose.pose.orientation.y = 0.707;
+		end_pose.pose.orientation.z = 0.0;
 
-end_pose.pose.position.z += 0.10;
-end_pose.pose.orientation.w = 0.707;
-end_pose.pose.orientation.x = 0.0;
-end_pose.pose.orientation.y = 0.707;
-end_pose.pose.orientation.z = 0.0;
+		tf2::doTransform(current_pose, end_pose, tfStamped);
 
-tf2::doTransform(current_pose, end_pose, tfStamped);
-
+		// Set the desired pose for the arm in the arm controller.
+		move_group.setPoseTarget(end_pose);
+		// Instantiate and create a plan.
+		moveit_::planning_interface::MoveGroupInterface::Plan the_plan;
+		// Create a plan based on the settings (all default settings now) in the_plan.
+		move_group.plan(the_plan);
+		// Planning does not always succeed. Check the output.
+		// In the event that the plan was created, execute it.
+		move_group.execute(the_plan);
+	}
   ros::spin();
 
   return 0;
